86204531dd61e2ba558691a488bf67ecf2d221f9
Pipe22Px <s6501012630166@email.kmutnb.ac.th>
2024-12-21 16:03:38 +0700

    Delete src directory

--- src/context/handler-context.js
+++
@@ -1,7 +0,0 @@
-import { createContext } from "react";
-
-export const HandlerContext = createContext({
-    addNewTodo: (newTodo) => {},
-    deleteHandler: (id) => {},
-    editHandler: (id,todo) => {},
-})
